f508c6aacd0023e4d777d286dcc9671c4c5974fc
Colum Peters <g00427223@atu.ie>
2023-12-11 16:44:40 +0000

    Made ShiftWorker.java Class, with shift, payroll number and hourly pay fields Added test code to main, to ensure ShiftWorker class works correctly

---
+++ src/main/java/ShiftWorker.java
@@ -0,0 +1,46 @@
+public class ShiftWorker extends Employee{
+    private int shift;
+    private int payNum;
+    private double ratePay;
+
+    public ShiftWorker(){
+        this.shift = 0;
+        this.payNum = 0;
+        this.ratePay =0;
+    }
+
+    public ShiftWorker(String name, int payNum, int shift, double ratePay, String userID){
+        super(name, userID);
+        this.shift = shift;
+        this.payNum = payNum;
+        this.ratePay = ratePay;
+    }
+
+    public int getShift() {
+        return shift;
+    }
+
+    public void setShift(int shift) {
+        this.shift = shift;
+    }
+
+    public int getPayNum() {
+        return payNum;
+    }
+
+    public void setPayNum(int payNum) {
+        this.payNum = payNum;
+    }
+
+    public double getRatePay() {
+        return ratePay;
+    }
+
+    public void setRatePay(double ratePay) {
+        this.ratePay = ratePay;
+    }
+
+    public void getReportDetails(){
+        System.out.println("Name: " + getName() + "\n" + "Payroll Number: " + payNum + "\n" + "Shift: " + shift + "\n" + "Hourly Pay: " + ratePay);
+    }
+}
